9f7e57ab7e62b9b9bb057f7d80dd6bc3353bd5ad
TheDuf <m.dufner@yahoo.com>
2016-10-23 16:52:27 -0400

    Add files via upload
    My registration form and all the php stuff needed to actually save the submitted information

---
+++ simfatic-RegistrationForm-dc288cf/source/register.php
@@ -0,0 +1,85 @@
+<?PHP
+require_once("./include/membersite_config.php");
+
+if(isset($_POST['submitted']))
+{
+   if($fgmembersite->RegisterUser())
+   {
+        $fgmembersite->RedirectToURL("thank-you.html");
+   }
+}
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+<head>
+    <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+    <title>Contact us</title>
+    <link rel="STYLESHEET" type="text/css" href="style/fg_membersite.css" />
+    <script type='text/javascript' src='scripts/gen_validatorv31.js'></script>
+    <link rel="STYLESHEET" type="text/css" href="style/pwdwidget.css" />
+    <script src="scripts/pwdwidget.js" type="text/javascript"></script>      
+</head>
+<body>
+
+<!-- Form Code Start -->
+<div id='fg_membersite'>
+<form id='register' action='<?php echo $fgmembersite->GetSelfScript(); ?>' method='post' accept-charset='UTF-8'>
+<fieldset >
+<legend>Register</legend>
+
+<input type='hidden' name='submitted' id='submitted' value='1'/>
+
+<div class='short_explanation'>* required fields</div>
+<input type='text'  class='spmhidip' name='<?php echo $fgmembersite->GetSpamTrapInputName(); ?>' />
+
+<div><span class='error'><?php echo $fgmembersite->GetErrorMessage(); ?></span></div>
+<div class='container'>
+    <label for='name' >First Name*: </label><br/>
+    <input type='text' name='firstname' id='firstname' value='<?php echo $fgmembersite->SafeDisplay('firstname') ?>' maxlength="50" /><br/>
+    <span id='register_firstname_errorloc' class='error'></span>
+</div>
+<div class='container'>
+    <label for='name' >Last Name*: </label><br/>
+    <input type='text' name='lastname' id='lastname' value='<?php echo $fgmembersite->SafeDisplay('lastname') ?>' maxlength="50" /><br/>
+    <span id='register_lastname_errorloc' class='error'></span>
+</div>
+<div class='container'>
+    <label for='email' >Email Address*:</label><br/>
+    <input type='text' name='email' id='email' value='<?php echo $fgmembersite->SafeDisplay('email') ?>' maxlength="50" /><br/>
+    <span id='register_email_errorloc' class='error'></span>
+</div>
+<div class='container'>
+    <input type='submit' name='Submit' value='Submit' />
+</div>
+
+</fieldset>
+</form>
+<!-- client-side Form Validations:
+Uses the excellent form validation script from JavaScript-coder.com-->
+
+<script type='text/javascript'>
+// <![CDATA[
+    var pwdwidget = new PasswordWidget('thepwddiv','password');
+    pwdwidget.MakePWDWidget();
+    
+    var frmvalidator  = new Validator("register");
+    frmvalidator.EnableOnPageErrorDisplay();
+    frmvalidator.EnableMsgsTogether();
+    frmvalidator.addValidation("firstname","req","Please provide your firstname");
+
+    frmvalidator.addValidation("lastname","req","Please provide your lastname");
+    
+    frmvalidator.addValidation("email","req","Please provide your email address");
+
+    frmvalidator.addValidation("email","email","Please provide a valid email address");
+
+// ]]>
+</script>
+
+<!--
+Form Code End (see html-form-guide.com for more info.)
+-->
+
+</body>
+</html>
